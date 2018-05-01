4a95a5ea82e83098c007ffa35bd408f0e47f3619
AldoS <sinani_aldo@hotmail.com>
2018-05-01 20:50:53 +0300

    changes on save data to db

--- Items_Move_Store/Form1.cs
+++ Items_Move_Store/Form1.cs
@@ -9,12 +9,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
+using System.Data.SQLite;
 
 namespace Items_Move_Store
 {
     public partial class Form1 : Form
     {
-
         public Form1()
         {
             InitializeComponent();
@@ -66,43 +66,92 @@ namespace Items_Move_Store
 
         private void button_Execute_Click(object sender, EventArgs e)
         {
-            string[,] Arr_Katastimatwn = new string[100, 10];
-            Arr_Katastimatwn = fillArray_Katastimatwn();
+            fillTables_fromExcel(textBox_File_Katastima.Text,"Stores");
+            fillTables_fromExcel(textBox_File_Kiniseis.Text, "Movements");
 
-            string[,] Arr_Kinisewn = new string[100, 10];
-            Arr_Kinisewn = fillArray_Kinisewn();
+            check_Items_in_Stores();
+
+            /*string[,] Arr_Katastimatwn;
+            Arr_Katastimatwn = fillArray_fromExcel(textBox_File_Katastima.Text);
+            string[,] Arr_Kinisewn;
+            Arr_Kinisewn = fillArray_fromExcel(textBox_File_Kiniseis.Text);*/
 
             //έλεγχος πόσα είδη απο αυτά που ζητήθηκαν θα βρεθούν στα διαθέσιμα καταστήματα
             //θα πάρουμε από αυτό στο οποίο υπάρχουν οι περισσότεροι κωδικοί ειδών
+            find_More_Items_In_Same_Store();
 
         }
 
-        private string[,] fillArray_Katastimatwn()
+        private void fillTables_fromExcel(String filePath, string tableName)
         {
-            string[,] Arr = new string[100, 10];
+            string startupPath = Environment.CurrentDirectory;
 
             //Create COM Objects. Create a COM object for everything that is referenced
             Excel.Application xlApp = new Excel.Application();
-            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"" + textBox_File_Katastima.Text);
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"" + filePath);
             Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
             Excel.Range xlRange = xlWorksheet.UsedRange;
 
             int rowCount = xlRange.Rows.Count;
             int colCount = xlRange.Columns.Count;
 
+            try
+            {
+                string connectionString = "URI=file: C:\\Users\\Aldo\\source\\repos\\Items_Move_Store\\Items_Move_Store\\database.sqlite";
+                using (SQLiteConnection conn = new SQLiteConnection(connectionString))
+                {
+                    conn.Open();
+                    string sql = "delete from " + tableName;
+                    SQLiteCommand command = new SQLiteCommand(sql, conn);
+                    command.ExecuteNonQuery();
+
+                    using (SQLiteCommand cmd = new SQLiteCommand())
+                    {
+                        for (int i = 2; i <= rowCount; i++)
+                        {
+                            string strSql = "INSERT INTO "+ tableName + " (Store,Item,Qty) VALUES(" + xlRange.Cells[i, 1].Value.ToString() + "," + xlRange.Cells[i, 2].Value.ToString() + "," + xlRange.Cells[i, 3].Value.ToString() + ")";
+                            cmd.CommandText = strSql;
+                            cmd.Connection = conn;
+
+                            //conn.Open();
+                            cmd.ExecuteNonQuery();
+                            // do something…
+                            //conn.Close();
+                        }
+                    }
+                    conn.Close();
+                }
+            }
+            catch (SQLiteException e)
+            {
+                MessageBox.Show("Σφάλμα στην εισαγωγή δεδομένων στη βάση: "+e.Message);
+            }
+
+            //close and release
+            xlWorkbook.Close();
+        }
+
+        private string[,] fillArray_fromExcel(String filePath)
+        {
+            //Create COM Objects. Create a COM object for everything that is referenced
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"" + filePath);
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            int rowCount = xlRange.Rows.Count+1;
+            int colCount = xlRange.Columns.Count+1;
+
+            string[,] Arr = new string[rowCount, colCount];            
+
             //iterate over the rows and columns and print to the console as it appears in the file
             //excel is not zero based!!
-            for (int i = 1; i <= rowCount; i++)
+            for (int i = 1; i < rowCount; i++)
             {
-                for (int j = 1; j <= colCount; j++)
+                for (int j = 1; j < colCount; j++)
                 {
-                    //new line
-                    if (j == 1)
-                        Console.Write("\r\n");
-
                     if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value != null)
                     {
-                        //Console.Write(xlRange.Cells[i, j].Value2.ToString() + "\t");
                         Arr[i, j] = xlRange.Cells[i, j].Value.ToString();
                     }
 
@@ -115,42 +164,47 @@ namespace Items_Move_Store
             return Arr;
         }
 
-        private string[,] fillArray_Kinisewn()
+        private void check_Items_in_Stores()
         {
-            string[,] Arr = new string[100, 10];
-
-            //Create COM Objects. Create a COM object for everything that is referenced
-            Excel.Application xlApp = new Excel.Application();
-            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"" + textBox_File_Kiniseis.Text);
-            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
-            Excel.Range xlRange = xlWorksheet.UsedRange;
-
-            int rowCount = xlRange.Rows.Count;
-            int colCount = xlRange.Columns.Count;
-
-            //iterate over the rows and columns and print to the console as it appears in the file
-            //excel is not zero based!!
-            for (int i = 1; i <= rowCount; i++)
+            //θα πάρουμε τα καταστήματα ταξινομημένα βάση των αριθμών των κινήσεων
+            string vSql = "select *, "+
+                  "  ("+
+                  "     select count(*) as kiniseis from Movements"+
+                  "     where M.Store = Store"+
+                  "     group by Store"+
+                  "  ) as kiniseis"+
+                  "  from Movements M"+
+                  "  order by kiniseis desc  ";
+            try
             {
-                for (int j = 1; j <= colCount; j++)
+                string connectionString = "URI=file: C:\\Users\\Aldo\\source\\repos\\Items_Move_Store\\Items_Move_Store\\database.sqlite";
+                using (SQLiteConnection conn = new SQLiteConnection(connectionString))
                 {
-                    //new line
-                    if (j == 1)
-                        Console.Write("\r\n");
-
-                    if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value != null)
+                    conn.Open();
+                    SQLiteCommand cmd = new SQLiteCommand(vSql, conn);
+                    SQLiteDataAdapter da = new SQLiteDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    int ItemId;
+                    foreach (DataRow dr in dt.Rows)
                     {
-                        //Console.Write(xlRange.Cells[i, j].Value2.ToString() + "\t");
-                        Arr[i, j] = xlRange.Cells[i, j].Value.ToString();
+                        //string asdasdasd = dr["Store"].ToString();
+                        ItemId = Int32.Parse(dr["Item"].ToString());
+                        vSql = "select * from Stores where Item = '"+ ItemId + "'";
+                        //θα γεμίσουμε πίνακα με όπου βρούμε το item που θέλουμε και σε τι ποσοστό
                     }
-
+                    conn.Close();
                 }
             }
+            catch (SQLiteException e)
+            {
+                MessageBox.Show("Σφάλμα στην εισαγωγή δεδομένων στη βάση: " + e.Message);
+            }
+        }
 
-            //close and release
-            xlWorkbook.Close();
+        private void find_More_Items_In_Same_Store()
+        {
 
-            return Arr;
         }
 
         private void createExcel()
